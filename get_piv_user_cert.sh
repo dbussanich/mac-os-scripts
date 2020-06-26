7dff06e512012184328c2621d77e7f1d37d8e3fa
Daniel Bussanich <m1dab01@Daniels-iMac.local>
2020-06-26 18:04:51 -0400

    tweaked script for UPN only

--- get_piv_user_cert.sh
+++ get_piv_user_cert.sh
@@ -17,21 +17,19 @@ UPN=`/usr/bin/openssl x509 -noout -text -in /tmp/temp.pem | awk -F ':' '/email/ 
 COMMON_NAME=$(/usr/bin/openssl asn1parse -i -dump -in /tmp/temp.pem | awk -F ':' '/commonName/ {getline; print $4}')
 
 # Use dscl command to query Open Directory to find user name with grep
-# Then, get real name from user name
+# Then, get real name from user name, convert to upper case
 USERNAME=$(dscl . -list /Users | grep -E "m(1|3)\w\w\w\d\d")
-REAL_NAME=$(dscl . -read /Users/${USERNAME} RealName)
+REAL_NAME=$(dscl . -read /Users/${USERNAME} RealName | awk '/RealName/{getline; print}' \
+| sed 's/^ *//g' | tr '[:lower:]' '[:upper:]')
 
-# Convert user name to all upper case
 # Then compare Common Name to real name upper. If not equivalent, then wrong smart card is entered
-REAL_NAME_UPPER=`echo $REAL_NAME | awk '/RealName/{getline; print}' | sed 's/^ *//g' | tr '[:lower:]' '[:upper:]'`
 
-
-if [ "$COMMON_NAME" != "$REAL_NAME_UPPER" ]; then
-    echo "$COMMON_NAME NOT MATCHING WITH $REAL_NAME_UPPER"
+if [ "$COMMON_NAME" != "$REAL_NAME" ]; then
+    echo "$COMMON_NAME NOT MATCHING WITH $REAL_NAME"
     return 1
 fi
 
 # Add attribute to user's Open Directory account for smartcard attribute matching
 ## To do, the sudo is weird here, but I coudln't get it to run with sudo privs without putting it here.
 ## I thought that the whole script would run with sudo privs without it.
-sudo dscl . -append /Users/${USERNAME} dsAttrTypeNative:smartCardIdentity "$COMMON_NAME - $UPN"
+sudo dscl . -append /Users/${USERNAME} dsAttrTypeNative:smartCardIdentity "$UPN"
