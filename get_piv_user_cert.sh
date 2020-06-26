b050717489c29aef7964474619d214c6a4f10045
Daniel Bussanich <m1dab01@Daniels-iMac.local>
2020-06-26 12:07:19 -0400

    Compare CN to Realname, exit if not equal

--- get_piv_user_cert.sh
+++ get_piv_user_cert.sh
@@ -17,10 +17,17 @@ UPN=`/usr/bin/openssl x509 -noout -text -in /tmp/temp.pem | awk -F ':' '/email/ 
 COMMON_NAME=$(/usr/bin/openssl asn1parse -i -dump -in /tmp/temp.pem | awk -F ':' '/commonName/ {getline; print $4}')
 
 # Use dscl command to query Open Directory to find user name with grep
+# Then, get real name from user name
 USERNAME=$(dscl . -list /Users | grep -E "m(1|3)\w\w\w\d\d")
+REAL_NAME=$(dscl . -read /Users/${USERNAME} RealName)
+
+# Convert user name to all upper case
+# Then compare Common Name to real name upper. If not equivalent, then wrong smart card is entered
+REAL_NAME_UPPER=`echo $REAL_NAME | awk '/RealName/{getline; print}' | sed 's/^ *//g' | tr '[:lower:]' '[:upper:]'`
+
+if [ "$COMMON_NAME" != "$REAL_NAME_UPPER" ]; then
+    return 1
+fi
 
 # Add attribute to user's Open Directory account for smartcard attribute matching
 dscl . -append /Users/${USERNAME} dsAttrTypeNative:smartCardIdentity "$COMMON_NAME - $UPN"
-
-# To do
-# Prior to appending attribute, verify that Common Name matches RealName attribute in Open Directory
