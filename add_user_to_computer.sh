a04cbf58ed0313656ba3f38da446b73f9ba52481
Daniel Bussanich <dbussani@icloud.com>
2023-10-31 11:06:45 -0400

    Completed add new user script

--- add_user_to_computer.sh
+++ add_user_to_computer.sh
@@ -2,11 +2,13 @@
 
 # Parses a JSON of users and adds those users to a macOS computer
 
-while IFS=, read -r col1 col2 col3
+while IFS=, read -r EMAIL FULLNAME NEWUSER
 do
-    echo "Email: $col1"
-    echo "Full Name: $col2"
-    echo "Username: $col3"
-    echo ""
+    # Create a new user with a username
+    PASS=uuidgen
+    sysadminctl -addUser $NEWUSER -fullName $FULLNAME -password $PASS
+    echo "Added user $NEWUSER"
+    sleep 1
+    dscl . -append /Users/${NEWUSER} dsAttrTypeNative:smartCardIdentity "$EMAIL"
+    echo "Added email $EMAIL to $NEWUSER dsattribute"
 done
-
